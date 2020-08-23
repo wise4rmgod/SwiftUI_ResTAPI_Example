16211ac100c1c289a6e60fcbb7cee0be703777f8
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2020-08-23 14:02:18 +0100

    Initial Commit

---
+++ SwiftUI_ResTAPI_Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ResTAPI_Example
+//
+//  Created by wisdom nwokocha on 23/08/2020.
+//  Copyright © 2020 wisdom nwokocha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
